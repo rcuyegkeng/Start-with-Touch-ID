1f32560486c4e16eb8ae77f52bfe93a57ae2183d
Robert Cuyegkeng <rcuyegkeng@gmail.com>
2017-02-16 11:46:52 -0500

    Initial Commit

---
+++ Start with Touch ID/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Start with Touch ID
+//
+//  Created by Robert Cuyegkeng on 2017-02-16.
+//  Copyright © 2017 Robert Cuyegkeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
